edb458f833a0239ed504414d392e75c312a9290c
nurbanii <71011375+nurbanii@users.noreply.github.com>
2020-09-11 17:11:47 +0700

    Membuat class Anjing.

--- index.php
+++ index.php
@@ -10,11 +10,27 @@ $Kucing = new Kucing;
 $Kucing->Nama = "Hecy";
 $Kucing->Jumlah_Kaki = "4";
 $Kucing->Bisa_Terbang = "Tidak";
-$Kucing->Suara = "Meoong";
+$Kucing->Suara = "Meoong Meoong";
 
 echo "Kucing <br>";
 echo "Nama : $Kucing->Nama <br>";
 echo "Jumlah_Kaki : $Kucing->Jumlah_Kaki <br>";
 echo "Bisa_Terbang : $Kucing->Bisa_Terbang <br>";
 echo "Suara : $Kucing->Suara <br>";\
+echo "<hr>";
+
+class Anjing{
+	public $Nama, $Jumlah_Kaki, $Bisa_Terbang, $Suara;
+} 
+$Anjing = new Kucing;
+$Anjing->Nama = "Mety";
+$Anjing->Jumlah_Kaki = "4";
+$Anjing->Bisa_Terbang = "Tidak";
+$Anjing->Suara = "Guk Guk";
+
+echo "Anjing <br>";
+echo "Nama : $Anjing->Nama <br>";
+echo "Jumlah_Kaki : $Anjing->Jumlah_Kaki <br>";
+echo "Bisa_Terbang : $Anjing->Bisa_Terbang <br>";
+echo "Suara : $Anjing->Suara <br>";\
 echo "<hr>";
